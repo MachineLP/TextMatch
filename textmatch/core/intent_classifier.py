409fd0aa96181058dc14afebb2dca1a4fdfa2efb
MachineLP <MachineLP@163.com>
2020-06-22 10:48:23 +0800

    优化分本分类

--- textmatch/core/intent_classifier.py
+++ textmatch/core/intent_classifier.py
@@ -10,21 +10,35 @@
 
 import json 
 import numpy as np
-from textmatch.models.text_embedding.model_factory_sklearn import ModelFactory
-from textmatch.models.text_classifier.dnn import DNN 
+from textmatch.models.text_embedding.model_factory_sklearn import ModelFactory as ModelFactory_emb
+from textmatch.models.text_classifier.model_factory import ModelFactory as ModelFactory_cls
 
 
 class IntentClassifier():
 
-    def __init__( self, model_factory=ModelFactory, match_models=['bow'] ):
-        self.model_factory = model_factory
+    def __init__( self, model_factory_emb=ModelFactory_emb, model_factory_cls=ModelFactory_cls, match_models=['bow'] ):
+        self.model_factory_emb = model_factory_emb
+        self.model_factory_cls = model_factory_cls
         self._init_model( self.words_dict,  match_models)
 
     def _init_model(self, match_models=['bow', 'tfidf', 'ngram_tfidf']):
-        self.mf = self.model_factory( match_models=words_dict )
+        self.mf_emb = self.model_factory_emb( match_models=words_dict ) 
+        self.mf_cls = self.model_factory_cls( match_models=words_dict_ ) 
     
+    def predict_emb(self, words, word_id=None):
+        return self.mf_emb.predict_emb(words, word_id)
+    
+    def predict_cls(self, words, word_id=None):
+        return self.mf_cls.predict_cls(words)
+
+
     def predict(self, words, word_id=None):
-        return self.model.predict_emb(words, word_id)
+        emb = self.predict_emb( words ) 
+        words_emb=[]
+        for per_v in emb.values():
+           words_emb.extend( per_v )
+        words_emb = np.array(words_emb)
+        return self.predict_cls( words_emb ) 
 
 
 
--- textmatch/models/text_classifier/model_factory.py
+++ textmatch/models/text_classifier/model_factory.py
@@ -13,6 +13,11 @@ import logging
 import numpy as np
 from textmatch.config.constant import Constant as const
 from textmatch.models.text_classifier.dnn import DNN 
+from textmatch.models.ml.lr import LR
+from textmatch.models.ml.gbdt import GBDT
+from textmatch.models.ml.gbdtlr import GBDTLR
+from textmatch.models.ml.xgb import XGB
+from textmatch.models.ml.lgb import LGB
 
 '''
 '''
@@ -20,20 +25,41 @@ from textmatch.models.text_classifier.dnn import DNN
 class ModelFactory(object):
     '''match model factory
     '''
-    def __init__(self, match_models=['dnn', 'cnn', 'rnn'],
-                       bow_model = DNN,
+    def __init__(self, match_models=['gbdt']
                        ):
-       self.model = {}
-       for match_model in match_models:
-           if match_model == 'dnn':
+        self.model = {}
+        for match_model in match_models:
+            if match_model == 'lr':
+                model = LR()
+                model.load_model()
+                self.model[match_model] = model
+            elif match_model == 'gbdt':
+                model = GBDT()
+                model.load_model()
+                self.model[match_model] = model
+            elif match_model == 'gbdtlr':
+                model = GBDTLR()
+                model.load_model()
+                self.model[match_model] = model
+            elif match_model == 'xgb':
+                model = XGB()
+                model.load_model()
+                self.model[match_model] = model
+            elif match_model == 'xlgb':
+                model = LGB()
+                model.load_model()
+                self.model[match_model] = model
+            else:
+                logging.error( "[text classifer ModelFactory] match_model not existed，please select from ['dnn', 'rnn', 'cnn', ...] " )
+                continue
+    def init(self):
+        pass
 
-           else:
-               logging.error( "[text classifer ModelFactory] match_model not existed，please select from ['dnn', 'rnn', 'cnn', ...] " )
-               continue
-
-   
-     
-    
-
-
+    def predict(self, words):
+        pre_dict = {}
+        for key, model in self.model.items():
+            pre_list = []
+            pre = model.predict(words)
+            pre_dict[key] = pre
+        return pre_dict 
 
